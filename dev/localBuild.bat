034aaa3b2ca8b8edf015761b285e34c8821a5d96
Scott W Harden <swharden@gmail.com>
2021-09-26 15:10:39 -0400

    local server with Docker

---
+++ dev/localBuild.bat
@@ -0,0 +1,6 @@
+dotnet run ^
+  --project %~dp0\SiteBuilder ^
+  --content %~dp0\..\content ^
+  --theme %~dp0\theme ^
+  --urlSource https://github.com/ScottPlot/ScottPlot.NET/blob/main/content ^
+  --urlSite http://localhost:8080 ^
---
+++ dev/localServe.bat
@@ -0,0 +1,2 @@
+:: docker must be running or you'll get a docker.errors.DockerException
+docker-compose -f "%~dp0\docker\docker-compose.yml" up -d
