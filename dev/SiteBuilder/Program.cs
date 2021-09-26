b10678733ee2c3a068989093bdf573c2ec21d0f1
Scott W Harden <swharden@gmail.com>
2021-09-26 15:10:26 -0400

    SiteBuilder

---
+++ dev/SiteBuilder/Program.cs
@@ -0,0 +1,38 @@
+﻿using CommandLine;
+
+namespace SiteBuilder
+{
+    class Program
+    {
+        private class CommandLineOptions
+        {
+            [Option(longName: "content", Required = true, HelpText = "path of the root website (containing markdown files)")]
+            public string Content { get; set; }
+
+            [Option(longName: "theme", Required = true, HelpText = "path of the theme folder (containing HTML templates)")]
+            public string Theme { get; set; }
+
+            [Option(longName: "urlSource", Required = true, HelpText = "URL of the content source code")]
+            public string SourceUrl { get; set; }
+
+            [Option(longName: "urlSite", Required = true, HelpText = "URL of the content on the web")]
+            public string SiteUrl { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(RunOptions);
+        }
+
+        static void RunOptions(CommandLineOptions opts)
+        {
+            var ssg = new Statix.Generator(
+                contentFolder: opts.Content,
+                themeFolder: opts.Theme,
+                sourceUrl: opts.SourceUrl,
+                rootUrl: opts.SiteUrl);
+
+            ssg.Generate();
+        }
+    }
+}
