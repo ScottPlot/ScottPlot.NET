bd6f668d74dc4b13e097e0519a659aff1272ffbf
Scott W Harden <swharden@gmail.com>
2021-09-26 18:34:55 -0400

    create cookbook generator module

---
+++ dev/SiteBuilder/CookbookGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteBuilder
+{
+    public class CookbookGenerator
+    {
+        public CookbookGenerator(string cookbookCsprojFilePath)
+        {
+            if (!cookbookCsprojFilePath.EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase))
+                throw new ArgumentException("not a csproj file path", nameof(cookbookCsprojFilePath));
+
+            if (!File.Exists(cookbookCsprojFilePath))
+                throw new ArgumentException($"csproj file not found: {cookbookCsprojFilePath}", nameof(cookbookCsprojFilePath));
+
+            cookbookCsprojFilePath = Path.GetFullPath(cookbookCsprojFilePath);
+
+            Console.WriteLine($"analyzing cookbook: {cookbookCsprojFilePath}");
+        }
+
+        public void Generate()
+        {
+            Console.WriteLine("generating...");
+        }
+    }
+}
