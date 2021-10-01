87a3507cd68618cdc2002b7a0c449241e7d93376
Scott W Harden <swharden@gmail.com>
2021-10-01 00:48:50 -0400

    dev: copy scottplot dev folder

--- dev/01-copy.bat
+++ dev/01-copy.bat
@@ -1,4 +1,5 @@
-:: GitHub Actions checks-out the latest ScottPlot from the web.
-:: This script copies it from a local source.
+:: GitHub Actions checks-out the latest ScottPlot from the web and uses it to generate the cookbook.
+:: Instead of cloning the repo over HTTP this script copies the useful folders over from a local source.
 rmdir %~dp0\..\ScottPlot\src\ /s /q
 xcopy %~dp0\..\..\ScottPlot\src\ %~dp0\..\ScottPlot\src\ /E/H
+xcopy %~dp0\..\..\ScottPlot\dev\ %~dp0\..\ScottPlot\dev\ /E/H
