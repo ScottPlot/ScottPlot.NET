6449a69953d4d5607c0dd531317db80b568c57a5
Scott W Harden <swharden@gmail.com>
2021-10-10 19:57:46 -0400

    create contributors page and improve user icons

--- dev/03-build.bat
+++ dev/03-build.bat
@@ -1,3 +1,5 @@
+dotnet test %~dp0\SiteTests
+
 dotnet run ^
   --project %~dp0\SiteBuilder ^
   --content %~dp0\..\content ^
--- dev/SiteTests/Generate.cs
+++ dev/SiteTests/Generate.cs
@@ -16,11 +16,15 @@ namespace SiteTests
         [Test]
         public void Test_Generate_LocalSite()
         {
-            // create updated changelog file
+            // updated changelog and contributors markdown files
             string rawMarkdown = ReadLocalChangelog() ?? DownloadChangelog();
             string changelogMarkdownRaw = GetAnnotatedChangelogMarkdown(rawMarkdown);
             string changelogIndexPagePath = Path.Combine(PATH_REPO_ROOT, "content/changelog/index.md");
             CreateChangelogMarkdownPage(changelogMarkdownRaw, changelogIndexPagePath);
+
+            string[] contributors = GetAllUsernames(rawMarkdown);
+            string contributorsIndexPagePath = Path.Combine(PATH_REPO_ROOT, "content/contributors/index.md");
+            CreateContributorsMarkdownPage(contributors, contributorsIndexPagePath);
 
             // generate updated cookbook pages
             /*
@@ -66,6 +70,21 @@ namespace SiteTests
             }
         }
 
+        private string[] GetAllUsernames(string rawMarkdown)
+        {
+            HashSet<string> uniqueNames = new();
+
+            foreach (string line in rawMarkdown.Split("\n"))
+            {
+                foreach (string username in GitHubMarkdown.GetUsernames(line))
+                {
+                    uniqueNames.Add(username);
+                }
+            }
+
+            return uniqueNames.OrderBy(x => x.ToLower()).ToArray();
+        }
+
         private string GetAnnotatedChangelogMarkdown(string rawMarkdown)
         {
             string[] chunks = rawMarkdown.Split("\n#");
@@ -74,30 +93,23 @@ namespace SiteTests
                 string md = chunks[i];
 
                 string[] usernames = GitHubMarkdown.GetUsernames(md);
-                md = GitHubMarkdown.LinkUsernames(md, usernames);
+                string[] uniqueUsernames = usernames.Append("swharden").Distinct().OrderBy(x => x).ToArray();
+                md = GitHubMarkdown.LinkUsernames(md, uniqueUsernames);
 
                 int[] issueNumbers = GitHubMarkdown.GetIssues(md);
                 md = GitHubMarkdown.LinkIssues(md, issueNumbers);
 
-                if (usernames.Length > 0)
+                if (uniqueUsernames.Length > 1) // will always contain swharden
                 {
                     md += "\n\n";
                     md += "<div class='text-center mt-4'>";
-                    md += "<div><i>Special thanks to this version's contributors!</i></div>";
-                    md += "<div>";
-                    foreach (string username in usernames.Append("swharden").Distinct().OrderBy(x => x))
-                    {
-                        md += $"<div class='d-inline-block mx-3'>";
 
-                        md += $"<a href='https://github.com/{username}'>" +
-                            $"<img src='https://github.com/{username}.png?size=100' height='100' width='100' />" +
-                            $"</a>";
-                        md += $"<br>";
-                        md += $"<a href='https://github.com/{username}'>@{username}</a>";
-
-                        md += "</div>";
-                    }
+                    md += "<div class='fs-4 fw-lighter text-center mx-5 px-5'>";
+                    md += "Special thanks to this version's contributors!";
                     md += "</div>";
+
+                    md += GetUserIconDiv(uniqueUsernames);
+
                     md += "</div>";
                 }
 
@@ -109,6 +121,40 @@ namespace SiteTests
             return string.Join("\n#", chunks);
         }
 
+        private string GetUserIconDiv(string[] usernames)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("<div class='d-flex justify-content-center flex-wrap'>");
+            foreach (string username in usernames)
+            {
+                sb.AppendLine(GetUserIconDiv(username));
+            }
+            sb.AppendLine("</div>");
+            return sb.ToString();
+        }
+
+        private string GetUserIconDiv(string username)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"<div class='bg-light border rounded text-center mx-3 my-2'>");
+
+            sb.AppendLine($"<div>");
+            sb.AppendLine($"<a href='https://github.com/{username}'>");
+
+            int px = 125;
+            sb.AppendLine($"<img src='https://github.com/{username}.png?size={px}' height='{px}' width='{px}' " +
+                          $"class='img-fluid rounded m-1' />");
+            sb.AppendLine($"</a>");
+            sb.AppendLine($"</div>");
+
+            sb.AppendLine($"<div>");
+            sb.AppendLine($"<a href='https://github.com/{username}'>@{username}</a>");
+            sb.AppendLine($"</div>");
+
+            sb.AppendLine($"</div>");
+            return sb.ToString();
+        }
+
         private void CreateChangelogMarkdownPage(string changelogMarkdown, string outputFilePath)
         {
             StringBuilder sb = new();
@@ -118,6 +164,30 @@ namespace SiteTests
             sb.AppendLine("Source: https://github.com/ScottPlot/ScottPlot/blob/master/dev/changelog.md");
             sb.AppendLine("---");
             sb.AppendLine(changelogMarkdown);
+
+            string outputFolder = Path.GetDirectoryName(outputFilePath);
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+        }
+
+        private void CreateContributorsMarkdownPage(string[] usernames, string outputFilePath)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("---");
+            sb.AppendLine("Title: ScottPlot Contributors");
+            sb.AppendLine("Description: GitHub users who contributed to ScottPlot");
+            sb.AppendLine("Source: https://github.com/ScottPlot/ScottPlot/blob/master/dev/changelog.md");
+            sb.AppendLine("---");
+
+            sb.AppendLine("<div class='display-3 text-center'>ScottPlot Contributors</div>");
+            sb.AppendLine("<div class='fs-3 fw-lighter text-center mx-5 px-5'>");
+            sb.AppendLine("Special thanks to all the people who contributed to discussions, issues, and pull requests on GitHub!");
+            sb.AppendLine("</div>");
+            sb.AppendLine("<div class='display-3 my-5 text-center'>💖🚀✨</div>");
+
+            sb.AppendLine(GetUserIconDiv(usernames));
 
             string outputFolder = Path.GetDirectoryName(outputFilePath);
             if (!Directory.Exists(outputFolder))
