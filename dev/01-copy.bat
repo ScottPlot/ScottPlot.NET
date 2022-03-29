257cd36d9b605009392bcf6d8707537b3cf7acaa
Scott W Harden <swharden@gmail.com>
2022-03-29 19:18:07 -0400

    delete Statix site generation system

--- dev/01-copy.bat
+++
@@ -1,5 +0,0 @@
-:: GitHub Actions checks-out the latest ScottPlot from the web and uses it to generate the cookbook.
-:: Instead of cloning the repo over HTTP this script copies the useful folders over from a local source.
-rmdir %~dp0\..\ScottPlot\src\ /s /q
-xcopy %~dp0\..\..\ScottPlot\src\ %~dp0\..\ScottPlot\src\ /E/H
-xcopy %~dp0\..\..\ScottPlot\dev\ %~dp0\..\ScottPlot\dev\ /E/H
--- dev/04-serve.bat
+++
@@ -1,2 +0,0 @@
-:: docker must be running or you'll get a docker.errors.DockerException
-docker-compose -f "%~dp0\docker\docker-compose.yml" up -d
