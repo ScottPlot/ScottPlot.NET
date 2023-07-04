19d3ceb9d4cf4f41e8983b56ccbd759e8dce74ea
Scott W Harden <swharden@gmail.com>
2023-07-04 18:18:25 -0400

    favicon: support transparency
    improve support for transparent PNG output
    
    also no longer needs to export intermediate PNG files to generate the multi-size ICO file
    
    resolves #27

--- static/images/brand/update.bat
+++ static/images/brand/update.bat
@@ -1,18 +1,13 @@
-:: banner.svg is a 1200x630 for og images
-:: magick banner.svg banner.png :: export from InkScape because font issue
-magick banner.png banner.jpg
+:: The banner is a 1200x630 OpenGraph image.
+:: Do not convert it to PNG using ImageMagick because font anti-aliasing issue.
+:: Instead export from InkScape because ImageMagick has issues
 
-:: favicon.svg is a square icon
-magick favicon.svg -resize 16x16 favicon-16.png
-magick favicon.svg -resize 32x32 favicon-32.png
-magick favicon.svg -resize 64x64 favicon-64.png
-magick favicon.svg -resize 128x128 favicon-128.png
-magick favicon.svg -resize 180x180 favicon-180.png
-magick favicon.svg -resize 192x192 favicon-192.png
-magick favicon.svg -resize 256x256 favicon-256.png
-magick favicon.svg -resize 512x512 favicon-512.png
-magick favicon-32.png -colors 256 favicon-32.ico
-magick favicon-16.png favicon-32.png favicon-64.png favicon-128.png favicon-256.png -colors 256 favicon.ico
+:: create manifest images
+magick -background none favicon.svg -resize 192x192 favicon-192.png
+magick -background none favicon.svg -resize 512x512 favicon-512.png
+
+:: create favicon
+convert -background none favicon.svg -define icon:auto-resize=64,48,32,16 favicon.ico
 
 copy favicon.ico ..\..\
 
