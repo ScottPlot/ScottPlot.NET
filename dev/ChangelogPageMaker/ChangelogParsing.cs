8e5e3993d0646a057e83c7302eca3809266b5219
Scott W Harden <swharden@gmail.com>
2023-07-09 11:29:58 -0400

    ChangelogPageMaker: start project

---
+++ dev/ChangelogPageMaker/ChangelogParsing.cs
@@ -0,0 +1,76 @@
+﻿namespace ChangelogPageMaker;
+
+internal static class ChangelogParsing
+{
+    /// <summary>
+    /// Given a full changelog, return blocks of markdown (one per section)
+    /// </summary>
+    public static List<string[]> GetSections(string text)
+    {
+        string[] lines = text.Split('\n');
+
+        List<string[]> sections = new();
+
+        List<string> sectionLines = new();
+
+        foreach (string line in lines)
+        {
+            // ignore top-level headings
+            if (line.StartsWith("# "))
+                continue;
+
+            if (line.Trim().Length == 0)
+            {
+                if (!sectionLines.Any())
+                    continue;
+
+                // end of section
+                sections.Add(sectionLines.ToArray());
+                sectionLines.Clear();
+            }
+            else
+            {
+                sectionLines.Add(line.Trim());
+            }
+        }
+
+        return sections;
+    }
+
+    /// <summary>
+    /// Given a line of markdown, return the formatted HTML which links to issue pages and usernames
+    /// </summary>
+    public static string GetChangeLineHTML(string line)
+    {
+        if (!line.StartsWith("* "))
+            throw new InvalidOperationException("expected a change line to start with '* '");
+        string html = line;
+
+        // remove '* '
+        html = html[2..];
+
+        return html;
+    }
+
+    public static string[] GetGithubIDs(string changelog)
+    {
+        HashSet<string> ids = new();
+
+        string[] flankingText = { ",", "(", ")", ".", ",", "\n", "_" };
+        foreach (string flank in flankingText)
+        {
+            changelog = changelog.Replace(flank, " ");
+        }
+
+        foreach (string item in changelog.Split(" "))
+        {
+            if (item.StartsWith("@"))
+            {
+                string id = item.Substring(1).Trim();
+                ids.Add(id);
+            }
+        }
+
+        return ids.ToArray();
+    }
+}
---
+++ dev/ChangelogPageMaker/ChangelogParsingTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ChangelogPageMaker;
+
+internal class ChangelogParsingTests
+{
+    [Test]
+    public void Test_Sections_AreFound()
+    {
+        List<string[]> sections = ChangelogParsing.GetSections(SampleChangelog.Text);
+        sections.Should().NotBeEmpty();
+
+        foreach (string[] section in sections)
+        {
+            Console.WriteLine(section[0]);
+            Console.WriteLine(section.Last());
+            Console.WriteLine("");
+        }
+    }
+
+    [Test]
+    public void Test_SampleData_ContainsAllContributors()
+    {
+        Console.WriteLine($"Sample data has {SampleChangelog.Contributors.Length} contributors");
+        foreach (string author in SampleChangelog.Contributors)
+        {
+            string githubID = "@" + author;
+            Assert.That(SampleChangelog.Text.Contains(githubID));
+        }
+    }
+
+    [Test]
+    public void Test_GithubIDs_AreAllFound()
+    {
+        string[] ids = ChangelogParsing.GetGithubIDs(SampleChangelog.Text);
+        Console.WriteLine($"Parsed data has {ids.Length} contributors");
+        foreach (string id in ids)
+        {
+            Assert.That(SampleChangelog.Contributors, Contains.Item(id));
+        }
+    }
+}
