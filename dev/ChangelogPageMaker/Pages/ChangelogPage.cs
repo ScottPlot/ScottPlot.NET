f25fe2036c8b06a6970ebb56c176d4356ae225ee
Scott W Harden <swharden@gmail.com>
2023-07-09 21:14:32 -0400

    changelog: remove obsolete text

--- dev/ChangelogPageMaker/Pages/ChangelogPage.cs
+++ dev/ChangelogPageMaker/Pages/ChangelogPage.cs
@@ -17,12 +17,6 @@ internal class ChangelogPage : HtmlPageBase
         sb.AppendLine(Changelog.GetHtml());
 
         sb.AppendLine("<div class='mt-5 mb-3 text-muted'>");
-        sb.AppendLine("Contributors are listed in the same order they appear in the changelog, ");
-        sb.AppendLine("with those submitting contributions most recently at the top. ");
-        sb.AppendLine("Contributor avatars are downloaded the first time they appear in the changelog. ");
-        sb.AppendLine("</div>");
-
-        sb.AppendLine("<div class='mb-5 text-muted'>");
         sb.AppendLine("If you changed your GitHub avatar and wish to update the image on this page, ");
         sb.AppendLine("go to <a href='https://github.com/ScottPlot/ScottPlot.NET/tree/main/static/images/contributors'>");
         sb.AppendLine("https://github.com/ScottPlot/ScottPlot.NET/tree/main/static/images/contributors</a> ");
