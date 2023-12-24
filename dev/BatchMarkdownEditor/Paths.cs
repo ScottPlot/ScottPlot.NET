f67ff5ba5aab7e0788f3f91819f680c66cbe634e
Scott W Harden <swharden@gmail.com>
2023-12-24 13:16:07 -0500

    dev: markdown inspector

---
+++ dev/BatchMarkdownEditor/Paths.cs
@@ -0,0 +1,24 @@
+﻿namespace BatchMarkdownEditor;
+
+public static class Paths
+{
+    public static string[] GetMarkdownFilePaths()
+    {
+        string repoRoot = Path.GetFullPath("../../../../../");
+
+        string licenseFilePath = Path.Combine(repoRoot, "LICENSE");
+        if (!File.Exists(licenseFilePath))
+            throw new FileNotFoundException();
+
+        string contentFolder = Path.Combine(repoRoot, "content");
+        if (!Directory.Exists(contentFolder))
+            throw new DirectoryNotFoundException();
+
+        return Directory
+            .GetFiles(contentFolder, "*.md", SearchOption.AllDirectories)
+            //.Where(x=>!x.Contains("\\cookbook\\4.0\\"))
+            //.Where(x => !x.Contains("\\cookbook\\4.1\\"))
+            //.Where(x => !x.Contains("\\cookbook\\5.0\\"))
+            .ToArray();
+    }
+}
---
+++ dev/BatchMarkdownEditor/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BatchMarkdownEditor;
+
+public static class Program
+{
+    public static void Main()
+    {
+        foreach (string path in Paths.GetMarkdownFilePaths())
+        {
+            string[] lines = File.ReadAllLines(path);
+            var urlLines = lines.Where(x => x.StartsWith("url: ", StringComparison.InvariantCultureIgnoreCase));
+            if (urlLines.Any())
+            {
+                string url = urlLines.Single()[4..];
+                if (!url.Contains(".html", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continue;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(path);
+                Console.WriteLine(url);
+            }
+        }
+    }
+}
